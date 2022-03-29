d5a4a9e072e96fbf8ca7b5f5ba77a4d1da2b6642
Jon Gardeazabal <jon.gardeazabal@opendeusto.es>
2022-03-29 11:32:05 +0200

    Database implementation - User

--- src/main/java/com/parking/serialization/UsuarioDAO.java
+++ src/main/java/com/parking/serialization/UsuarioDAO.java
@@ -1,5 +1,94 @@
 package com.parking.serialization;
 
-public class UsuarioDAO {
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.jdo.Extent;
+import javax.jdo.PersistenceManager;
+import javax.jdo.Query;
+import javax.jdo.Transaction;
+
+public class UsuarioDAO extends DataAccessObjectBase implements IDataAccessObject<Usuario> {
+
+	private static UsuarioDAO instance;	
+	
+	private UsuarioDAO() { }
+	
+	public static UsuarioDAO getInstance() {
+		if (instance == null) {
+			instance = new UsuarioDAO();
+		}		
+		
+		return instance;
+	}	
+	
+	@Override
+	public void save(Usuario object) {
+		super.saveObject(object);
+	}
+
+	@Override
+	public void delete(Usuario object) {
+		super.deleteObject(object);
+	}
+
+	@Override
+	public List<Usuario> getAll() {
+		PersistenceManager pm = pmf.getPersistenceManager();
+		Transaction tx = pm.currentTransaction();
+		
+		List<Usuario> usuarios = new ArrayList<>();
+
+		try {
+			tx.begin();
+			
+			Extent<Usuario> retExtent = pm.getExtent(Usuario.class, true);
+			
+			for (Usuario u : retExtent) {
+				usuarios.add(u);
+			}
+						
+			tx.commit();
+		} catch (Exception ex) {
+			System.out.println("  $ Error querying all challenges: " + ex.getMessage());
+		} finally {
+			if (tx != null && tx.isActive()) {
+				tx.rollback();
+			}
+
+			pm.close();
+		}
+
+		return usuarios;
+	}
+
+	@Override
+	public Usuario find(String param) {		
+		PersistenceManager pm = pmf.getPersistenceManager();
+		Transaction tx = pm.currentTransaction();
+
+		Usuario result = null; 
+
+		try {
+			tx.begin();
+			
+			Query<?> query = pm.newQuery("SELECT FROM " + Usuario.class.getName() + " WHERE nombre == '" + param + "'");
+			query.setUnique(true);
+			result = (Usuario) query.execute();
+			
+			tx.commit();
+		} catch (Exception ex) {
+			System.out.println("  $ Error querying a Challenge: " + ex.getMessage());
+		} finally {
+			if (tx != null && tx.isActive()) {
+				tx.rollback();
+			}
+
+			pm.close();
+		}
+
+		return result;
+	}
+
 
 }
