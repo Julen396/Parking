84a7b507d5bafe8debeff0c23d7108fcccc2e8ac
Julen396 <julen396@gmail.com>
2022-03-30 22:50:51 +0200

    configuracion servidor

--- src/main/java/com/parking/server/ParkingServer.java
+++ src/main/java/com/parking/server/ParkingServer.java
@@ -1,4 +1,24 @@
 package com.parking.server;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+
+/**
+ * Root resource (exposed at "myresource" path)
+ */
+@Path("/collector")
+@Produces(MediaType.APPLICATION_JSON)
+
 public class ParkingServer {
+	
+	@GET
+	@Path("/hello")
+	public Response sayHello() {
+		
+		return Response.ok("Hello").build();
+	}
 }
