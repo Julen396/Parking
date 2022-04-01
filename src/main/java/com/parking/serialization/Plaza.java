099493cfb0a76ae4846f96137499915ed983cd32
UnaiCL <unai.calegain@opendeusto.es>
2022-04-01 13:07:26 +0200

    Clase plaza agreagada

---
+++ src/main/java/com/parking/serialization/Plaza.java
@@ -0,0 +1,35 @@
+package com.parking.serialization;
+
+public class Plaza {
+    private int piso;
+    private String zona;
+    private int posicion;
+
+    private Coche coche;
+
+    /**
+     * @return devuelve el piso
+     */
+    private int getPiso() { return piso;}
+
+    /**
+    * @return devuelve la zona (A, B, C...)
+     */
+    private String getZona() { return zona;}
+
+    /**
+     * @return la posición exacta de la plaza, dentro de la zona y el piso
+     */
+    private int getPosicion() { return piso;}
+
+    /**
+     * @return el coche que tiene reservada la plaza
+     */
+    private Coche getCoche() { return coche;}
+
+    /**
+     *
+     * @param coche el nuevo coche que tiene reservada la plaza
+     */
+    private void setCoche(Coche coche) { this.coche = coche;}
+}
