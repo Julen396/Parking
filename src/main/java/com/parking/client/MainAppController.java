47f045ffe89537cb0dff5497015ed4eed2233eef
Julen396 <julen396@gmail.com>
2022-04-02 14:38:42 +0200

    cambios en ventana

--- src/main/java/com/parking/client/MainAppController.java
+++ src/main/java/com/parking/client/MainAppController.java
@@ -2,14 +2,18 @@ package com.parking.client;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 
 public class MainAppController {
     @FXML
     private Label welcomeText;
-
+    
+    @FXML
+    private TextField matricula;
+    
     @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+        welcomeText.setText("Matricula Anadida!");
     }
 }
