d1df8c63f7d828f10d838a810d11b3ec8cd2e857
Beñat Oribe <benat.oribe@opendeusto.es>
2022-04-02 18:17:43 +0200

    Ventana para añadir usuarios

---
+++ src/main/java/com/parking/client/Usuario.java
@@ -0,0 +1,52 @@
+package com.parking.client;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Usuario extends Application {
+
+    private Stage primaryStage;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("RegistroUsuario");
+
+        initRootLayout();
+
+    }
+
+    /**
+     * Initializes the root layout.
+     */
+    public void initRootLayout() {
+        try {
+            // Load root layout from fxml file.
+            FXMLLoader loader = new FXMLLoader();
+            Scene scene = new Scene((Parent) loader.load(getClass().getResource("usuarioWindow.fxml")));
+
+            // Show the scene containing the root layout.
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Returns the main stage.
+     * @return returns the stage
+     */
+    public Stage getPrimaryStage() {
+        return primaryStage;
+    }
+}
