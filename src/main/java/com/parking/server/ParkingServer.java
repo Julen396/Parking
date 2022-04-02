212144c22402221e2ec420e7b18cefdbf7a8b15e
Julen396 <julen396@gmail.com>
2022-04-02 13:29:10 +0200

    Metodos para anadir y obtener coches

--- src/main/java/com/parking/server/ParkingServer.java
+++ src/main/java/com/parking/server/ParkingServer.java
@@ -1,9 +1,13 @@
 package com.parking.server;
 
+import com.parking.serialization.Coche;
+
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.POST;
 
 /**
  * Root resource (exposed at "myresource" path)
@@ -20,6 +24,34 @@ public class ParkingServer{
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String getIt() {
-        return "Got it!";
+        return "Funciona!";
     }
+    
+	private CocheCollector cocheCollector;
+	public ParkingServer() {
+		cocheCollector = new CocheCollector();
+		
+		/*//Juego de ensayo
+		Coche coche1 = new Coche();
+		coche1.setMatricula("sds5645");
+		anadirCoche(coche1);
+		Coche coche2 = new Coche();
+		coche2.setMatricula("yhh8486");
+		anadirCoche(coche2);
+		*/
+	}
+	
+	@POST
+	@Path("/coches")
+	public Response anadirCoche(Coche coche) {
+		cocheCollector.addCoche(coche);
+		System.out.println("Coche recibido");
+		return Response.ok(coche.getMatricula()).build();
+	}
+
+	@GET
+	@Path("/coches")
+	public Response getCoches() {
+		return Response.ok(cocheCollector.getCoches()).build();
+	}
 }
