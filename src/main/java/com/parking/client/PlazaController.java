e4772b0bcbcf0ef3403c5b726952d2d34b533ab1
Beñat Oribe <benat.oribe@opendeusto.es>
2022-04-02 18:03:52 +0200

    Ventana para añadir plazas

---
+++ src/main/java/com/parking/client/PlazaController.java
@@ -0,0 +1,45 @@
+package com.parking.client;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+
+public class PlazaController {
+	
+	/** Buttons **/
+    @FXML
+    private Button botonAnyadir;
+    
+    /** TextFields **/
+    @FXML
+    private TextField coche;
+    @FXML
+    private TextField nombreZona;
+    @FXML
+    private TextField piso;
+    @FXML
+    private TextField posicion;    
+    
+    /** Labels **/
+    @FXML
+    private Label cocheText;
+    @FXML
+    private Label nombreZonaText;
+    @FXML
+    private Label pisoText;
+    @FXML
+    private Label posicionText;
+
+    /** Metodos **/
+    @FXML
+    void onPropietarioButtonClick(ActionEvent event) {
+    	cocheText.setText("coche anyadido");
+    	nombreZonaText.setText("zona anyadida!");
+    	pisoText.setText("piso anyadido!");
+    	posicionText.setText("posicion anyadida!");
+    }
+
+}
