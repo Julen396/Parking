fb5a508a7ec719cc64f65506926332b6516275f9
Beñat Oribe <benat.oribe@opendeusto.es>
2022-04-28 10:23:47 +0200

    Test usuario creados los métodos 'getFecha_nac' y 'setFecha_nac'

--- src/test/java/com/parking/serialization/UsuarioTest.java
+++ src/test/java/com/parking/serialization/UsuarioTest.java
@@ -3,6 +3,7 @@ package com.parking.serialization;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import java.util.Date;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -12,10 +13,14 @@ import org.junit.Test;
  */
 public class UsuarioTest {
     private Usuario u;
+    private Date d;
+    private Date d1;
 
     @Before
     public void setUp() {
-        u = new Usuario("1234567A", "Jon", null);
+        d = new Date(1995, 4,3);
+        d1 = new Date(1940, 11,30);
+        u = new Usuario("1234567A", "Jon", d);
     }
 
     @Test
@@ -41,19 +46,22 @@ public class UsuarioTest {
        String expected = "Iker";
        assertEquals(expected, u.getNombre());
     }
-/* 
+ 
     @Test
     public void testGetFecha_nac() {
-       
+       Date expected = new Date(1995, 4,3);
+       assertEquals(expected, u.getFecha_nac());
     }
     @Test
     public void testSetFecha_nac() {
-        
+        u.setFecha_nac(new Date(1940, 11,30));
+        Date expected = d1;
+        assertEquals(expected, u.getFecha_nac());
     }
-*/
+
     @Test
     public void testToString() {
-        String expected = "(DNI: 1234567A; nombre: Jon; fecha de nacimiento: null)";
+        String expected = "(DNI: 1234567A; nombre: Jon; fecha de nacimiento: " + d.toString() + ")";
         assertEquals(expected, u.toString());
     }
     
