e99c5a968210a01f9f69c95b330e64092edc7c05
Julen396 <julen396@gmail.com>
2022-05-17 18:35:16 +0200

    tests de parkingServer mejorado

--- src/test/java/com/parking/server/ParkingServerTest.java
+++ src/test/java/com/parking/server/ParkingServerTest.java
@@ -1,12 +1,14 @@
 package com.parking.server;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 import static org.mockito.Mockito.when;
 
 import java.sql.Date;
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -15,6 +17,7 @@ import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import com.parking.serialization.Coche;
+import com.parking.serialization.ListaUsuarios;
 import com.parking.serialization.Plaza;
 import com.parking.serialization.Usuario;
 
@@ -25,29 +28,35 @@ import jakarta.ws.rs.core.Response;
 public class ParkingServerTest {
 	
 	private Coche coche1;
-	//ParkingServer parkingServer;
 	private Usuario usuario;
 	private LocalDate fecha;
 	private Plaza plaza1;
-	
+	private ArrayList<Coche> coches;
+	private ListaUsuarios listaUsuarios;
 	
 	ParkingServer parkingServer;
+	
+	CocheCollector cocheCollector;
+	PlazaCollector plazaCollector;
+	UsuarioCollector usuarioCollector;
+	
 	@Before
 	public void setUp() {
 		coche1 = new Coche("1234ABC", usuario);
 		fecha = LocalDate.of(2000, Month.MAY, 15);
 		usuario = new Usuario("123456789A", "Aitor", fecha);
 		plaza1 = new Plaza(3, "C", 12, coche1);
-		parkingServer= org.mockito.Mockito.mock(ParkingServer.class);
+		cocheCollector= org.mockito.Mockito.mock(CocheCollector.class);
+		plazaCollector= org.mockito.Mockito.mock(PlazaCollector.class);
+		usuarioCollector= org.mockito.Mockito.mock(UsuarioCollector.class);
+		parkingServer=new ParkingServer(cocheCollector, plazaCollector, usuarioCollector);
 	}
 	@Test
 	public void testanadirCoche() {
 		
-		when(parkingServer.anadirCoche(coche1)).thenReturn(Response.ok(coche1, MediaType.APPLICATION_JSON).build());
-		
 		Response response = parkingServer.anadirCoche(coche1);
 		
-		assertEquals(coche1, response.getEntity());
+		assertEquals(coche1.getMatricula(), response.getEntity());
 	}
 	
 	@Test
@@ -56,7 +65,7 @@ public class ParkingServerTest {
 		ArrayList<Coche> coches = new ArrayList<>();
 		coches.add(coche1);	
 		
-		when(parkingServer.getCoches()).thenReturn(Response.ok(coches, MediaType.APPLICATION_JSON).build());
+		when(cocheCollector.getCoches()).thenReturn(coches);
 		
 		Response response = parkingServer.getCoches();
 		
@@ -65,8 +74,6 @@ public class ParkingServerTest {
 	
 	@Test
 	public void testanadirPlaza() {
-		
-		when(parkingServer.anadirPlaza(plaza1)).thenReturn(Response.ok(plaza1, MediaType.APPLICATION_JSON).build());
 		
 		Response response = parkingServer.anadirPlaza(plaza1);
 		
@@ -79,11 +86,25 @@ public class ParkingServerTest {
 		ArrayList<Plaza> plazas = new ArrayList<>();
 		plazas.add(plaza1);	
 		
-		when(parkingServer.getPlazas()).thenReturn(Response.ok(plazas, MediaType.APPLICATION_JSON).build());
+		when(plazaCollector.getPlazas()).thenReturn(plazas);
 		
 		Response response = parkingServer.getPlazas();
 		
 		assertEquals(plazas, response.getEntity());
+	}
+	
+	@Test
+	public void getUsuarios() {
+
+		ArrayList<Usuario> usuarios = new ArrayList<>();
+		usuarios.add(usuario);
+		
+		listaUsuarios = new ListaUsuarios(usuarios);
+		
+		when(usuarioCollector.getUsuarios()).thenReturn(usuarios);
+		
+		Response response = parkingServer.getUsuarios();
+		assertNotEquals(listaUsuarios, response.getEntity());
 	}
 	
 }
